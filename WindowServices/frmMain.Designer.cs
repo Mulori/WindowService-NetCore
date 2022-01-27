1f0b80031b71fe967ee316b53c368796320df019
mulori <murilogarcia.hr@gmail.com>
2022-01-27 18:02:55 -0300

    Primeiro commit

---
+++ WindowServices/frmMain.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WindowServices
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.list = new System.Windows.Forms.ListBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.Icon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnStart.FlatAppearance.BorderSize = 0;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Blair Caps", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(84, 26);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnStop.FlatAppearance.BorderSize = 0;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop.Font = new System.Drawing.Font("Blair Caps", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStop.Location = new System.Drawing.Point(102, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(86, 26);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // list
+            // 
+            this.list.FormattingEnabled = true;
+            this.list.ItemHeight = 15;
+            this.list.Location = new System.Drawing.Point(12, 45);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(546, 169);
+            this.list.TabIndex = 2;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 3000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnMinimize.FlatAppearance.BorderSize = 0;
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.Font = new System.Drawing.Font("Blair Caps", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnMinimize.Location = new System.Drawing.Point(445, 13);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(110, 26);
+            this.btnMinimize.TabIndex = 3;
+            this.btnMinimize.Text = "Minimize";
+            this.btnMinimize.UseVisualStyleBackColor = false;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // Icon
+            // 
+            this.Icon.Icon = ((System.Drawing.Icon)(resources.GetObject("Icon.Icon")));
+            this.Icon.Text = "WindowService RCR";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 220);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnMinimize);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Window Service - RCR Sistemas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnStart;
+        private Button btnStop;
+        private ListBox list;
+        private System.Windows.Forms.Timer timer;
+        private Button btnMinimize;
+        private NotifyIcon Icon;
+    }
+}
---
+++ WindowServices/frmMain.cs
@@ -0,0 +1,200 @@
+using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace WindowServices
+{
+    public partial class frmMain : Form
+    {
+        private static string nameService { get; set; }
+        private static string path { get; set; }
+        private static string task { get; set; }
+
+        List<string> listTarefas = new List<string>();
+        List<string> listCaminhos = new List<string>();
+
+        public frmMain()
+        {
+            InitializeComponent();
+            TrayMenuContext();
+
+            if (!Directory.Exists(@"C:\ServiceR\Config"))
+            {
+                Directory.CreateDirectory(@"C:\ServiceR\Config");
+            }
+
+            if (!File.Exists(@"C:\ServiceR\Config\nameService.ini"))
+            {
+                File.Create(@"C:\ServiceR\Config\nameService.ini");
+            }
+
+            if (!File.Exists(@"C:\ServiceR\Config\pathTask.ini"))
+            {
+                File.Create(@"C:\ServiceR\Config\pathTask.ini");
+            }
+
+            if (!File.Exists(@"C:\ServiceR\Config\nameTask.ini"))
+            {
+                File.Create(@"C:\ServiceR\Config\nameTask.ini");
+            }
+
+            try
+            {
+                nameService = File.ReadAllText(@"C:\ServiceR\Config\nameService.ini");
+                path = File.ReadAllText(@"C:\ServiceR\Config\pathTask.ini");
+                task = File.ReadAllText(@"C:\ServiceR\Config\nameTask.ini");
+            }
+            catch { }
+            
+
+            if (string.IsNullOrEmpty(nameService) || string.IsNullOrEmpty(path) || string.IsNullOrEmpty(task))
+            {
+                list.Items.Add("Não foi configurado o arquivo de servico, e o processo será finalizado");
+                list.Items.Add("");
+                list.Items.Add(@"Configure o nome do serviço em: C:\ServiceR\Config");
+
+                timer.Stop();
+
+                btnStart.Enabled = false;
+                btnStop.Enabled = false;
+
+                return;
+            }
+            
+            var paths = path.Split('|');
+            var tasks = task.Split('|');
+
+            foreach(var _task in tasks)
+            {
+                listTarefas.Add(_task);
+            }
+
+            foreach (var _path in paths)
+            {
+                listCaminhos.Add(_path);
+            }
+
+
+            list.Items.Clear();
+            list.Items.Add("Aguarde! Processando serviço solicitado...");
+
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+
+            OcultarForm();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+
+            timer.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+
+            timer.Stop();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            ServiceController sc = new ServiceController(nameService);
+
+            if ((sc.Status.Equals(ServiceControllerStatus.Stopped)) || (sc.Status.Equals(ServiceControllerStatus.StopPending)))
+            {
+                list.Items.Clear();
+                list.Items.Add("Processando Serviço Configurado...");
+                list.Items.Add($"O serviço ({nameService}) está parado... Ultima verificação: {DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}");
+
+                foreach(var i in listTarefas)
+                {
+                    if(i != "")
+                        Kill(i);
+                }
+            }
+            else
+            {
+                list.Items.Clear();
+                list.Items.Add("Processando Serviço Configurado...");
+                list.Items.Add($"O serviço ({nameService}) está em execução... Ultima verificação: {DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")}");
+
+                int index = 0;
+                foreach (var i in listCaminhos)
+                {
+                    if (i != "")
+                    {
+                        if (Process.GetProcessesByName(listTarefas[index]).Length < 1)
+                            Start(i);
+                    }
+
+                    index++;
+                }
+            }
+
+            sc.Refresh();
+        }
+
+        public void Kill(string pross)
+        {
+            try
+            {
+                Process[] ProcList;
+                ProcList = Process.GetProcessesByName(pross);
+                ProcList[0].Kill();
+            }
+            catch
+            {  }            
+        }
+
+        public void Start(string path)
+        {
+            Process.Start(path);
+        }
+
+        void AbrirForm()
+        {
+            this.Show();
+            this.ShowInTaskbar = true;
+            this.WindowState = FormWindowState.Normal;
+            Icon.Visible = false;
+        }
+
+        void OcultarForm()
+        {
+            this.Hide();
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            Icon.Visible = true;
+        }
+
+        public void TrayMenuContext()
+        {
+            this.Icon.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
+            this.Icon.ContextMenuStrip.Items.Add("Abrir", null, this.MenuOpen_Click);
+            this.Icon.ContextMenuStrip.Items.Add("Sair", null, this.MenuExit_Click);
+        }
+
+        void MenuOpen_Click(object sender, EventArgs e)
+        {
+            AbrirForm();
+        }
+
+        void MenuExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void btnMinimize_Click(object sender, EventArgs e)
+        {
+            OcultarForm();
+        }
+
+        public void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            AbrirForm();
+        }
+    }
+}
